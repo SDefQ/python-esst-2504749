bfac379117ef99b4e8042b733e816cba5cdac3a3
SDefQ <150050382+SDefQ@users.noreply.github.com>
2023-11-06 17:22:23 +0000

    new file:   uebungs_datai_2_5.py new file:   uebungs_datei_3_1.py

---
+++ uebungs_datai_2_5.py
@@ -0,0 +1,13 @@
+meine_liste = []
+
+meine_liste.append(0)
+meine_liste.append(0)
+meine_liste.append(0)
+meine_liste.append(0)
+meine_liste.append(0)
+
+print("Wert der Liste für Index 3: ", meine_liste[3])
+
+meine_liste[4] = -9
+print('Wert der Liste für Index 3: ', meine_liste[3])
+print('Wert der Liste für Index 4: ', meine_liste[4])
---
+++ uebungs_datei_3_1.py
@@ -0,0 +1,7 @@
+variable1 = 8
+variable2 = 8
+
+if(variable1 < variable2):
+  print('Die Variable_1 ist kleiner als die Variable 2.')
+else:
+  print('Die Variable 2 ist größer oder gleich der Variable 2.')
